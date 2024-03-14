918449512159d3f41539d813939cdc4f39280766
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-03-14 14:15:08 -0700

    JSONFieldWriterExternal introduced. Hidden handling. Refactoring of ValueConstraints creation and reading

---
+++ test/resources/templates/025/template-025.spec.ts
@@ -0,0 +1,58 @@
+import { JSONTemplateReader } from '../../../../src/org/metadatacenter/io/reader/JSONTemplateReader';
+import { ParsingResult } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ParsingResult';
+import { CedarJsonPath } from '../../../../src/org/metadatacenter/model/cedar/util/path/CedarJsonPath';
+import { TestUtil } from '../../../TestUtil';
+import { ComparisonError } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ComparisonError';
+import { ComparisonErrorType } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ComparisonErrorType';
+import { CedarModel } from '../../../../src/org/metadatacenter/model/cedar/constants/CedarModel';
+import { JSONTemplateReaderResult } from '../../../../src/org/metadatacenter/io/reader/JSONTemplateReaderResult';
+import { JSONTemplateWriter } from '../../../../src/org/metadatacenter/io/writer/JSONTemplateWriter';
+import { CedarWriters } from '../../../../src/org/metadatacenter/io/writer/CedarWriters';
+import { JsonSchema } from '../../../../src/org/metadatacenter/model/cedar/constants/JsonSchema';
+
+describe('JSONTemplateReader - template-025', () => {
+  test('reads template with controlled field', () => {
+    const templateSource = TestUtil.readTestResourceAsString('templates/025', 'template-025.json');
+    const reader: JSONTemplateReader = JSONTemplateReader.getStrict();
+    const jsonTemplateReaderResult: JSONTemplateReaderResult = reader.readFromString(templateSource);
+    expect(jsonTemplateReaderResult).not.toBeNull();
+    const parsingResult: ParsingResult = jsonTemplateReaderResult.parsingResult;
+    // TestUtil.p(parsingResult.getBlueprintComparisonErrors());
+    expect(parsingResult.wasSuccessful()).toBe(true);
+
+    const writers: CedarWriters = CedarWriters.getStrict();
+    const writer: JSONTemplateWriter = writers.getJSONTemplateWriter();
+
+    const compareResult: ParsingResult = JSONTemplateReader.getRoundTripComparisonResult(jsonTemplateReaderResult, writer);
+
+    // TestUtil.p(compareResult);
+    // TestUtil.p(writer.getAsJsonNode(jsonTemplateReaderResult.template));
+    // TestUtil.p(jsonTemplateReaderResult.template);
+
+    expect(compareResult.wasSuccessful()).toBe(false);
+    expect(compareResult.getBlueprintComparisonErrorCount()).toBe(3);
+
+    const uiPagesMissing = new ComparisonError(
+      'oco02',
+      ComparisonErrorType.MISSING_KEY_IN_REAL_OBJECT,
+      new CedarJsonPath(CedarModel.ui, CedarModel.pages),
+    );
+    expect(compareResult.getBlueprintComparisonErrors()).toContainEqual(uiPagesMissing);
+
+    const requiredControlled1Unexpected = new ComparisonError(
+      'oca02',
+      ComparisonErrorType.UNEXPECTED_VALUE_IN_REAL_OBJECT,
+      new CedarJsonPath(JsonSchema.properties, JsonSchema.atContext, JsonSchema.required, 11),
+      undefined,
+      'Hidden field',
+    );
+    expect(compareResult.getBlueprintComparisonErrors()).toContainEqual(requiredControlled1Unexpected);
+
+    const skosNotationControlled1Unexpected = new ComparisonError(
+      'oco01',
+      ComparisonErrorType.UNEXPECTED_KEY_IN_REAL_OBJECT,
+      new CedarJsonPath(JsonSchema.properties, 'Hidden field', JsonSchema.properties, JsonSchema.atLanguage),
+    );
+    expect(compareResult.getBlueprintComparisonErrors()).toContainEqual(skosNotationControlled1Unexpected);
+  });
+});

1a6c1820a468e085ca0e5ceea8e79d582471aed9
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-03-13 18:43:37 -0700

    Element in element test

---
+++ test/resources/elements/003/element-003.spec.ts
@@ -0,0 +1,58 @@
+import { ParsingResult } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ParsingResult';
+import { TestUtil } from '../../../TestUtil';
+import { ComparisonError } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ComparisonError';
+import { ComparisonErrorType } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ComparisonErrorType';
+import { CedarJsonPath } from '../../../../src/org/metadatacenter/model/cedar/util/path/CedarJsonPath';
+import { JsonSchema } from '../../../../src/org/metadatacenter/model/cedar/constants/JsonSchema';
+import { CedarWriters, JSONElementReader, JSONElementWriter } from '../../../../src';
+
+describe('JSONElementReader - element-003', () => {
+  test('reads an element with three fields', () => {
+    const templateSource = TestUtil.readTestResourceAsString('elements/003', 'element-003.json');
+    const reader: JSONElementReader = JSONElementReader.getStrict();
+    const jsonElementReaderResult = reader.readFromString(templateSource);
+    expect(jsonElementReaderResult).not.toBeNull();
+    const parsingResult = jsonElementReaderResult.parsingResult;
+    // TestUtil.p(parsingResult.getBlueprintComparisonErrors());
+    expect(parsingResult.wasSuccessful()).toBe(true);
+
+    const writers: CedarWriters = CedarWriters.getStrict();
+    const writer: JSONElementWriter = writers.getJSONElementWriter();
+
+    const compareResult: ParsingResult = JSONElementReader.getRoundTripComparisonResult(jsonElementReaderResult, writer);
+
+    // TestUtil.p(compareResult);
+    // TestUtil.p(writer.getAsJsonNode(jsonFieldReaderResult.field));
+
+    expect(compareResult.wasSuccessful()).toBe(false);
+    expect(compareResult.getBlueprintComparisonErrorCount()).toBe(2);
+
+    const languageTextfieldUnexpected = new ComparisonError(
+      'oco01',
+      ComparisonErrorType.UNEXPECTED_KEY_IN_REAL_OBJECT,
+      new CedarJsonPath(
+        JsonSchema.properties,
+        '#002 Element with three fields',
+        JsonSchema.properties,
+        'Text field',
+        JsonSchema.properties,
+        JsonSchema.atLanguage,
+      ),
+    );
+    expect(compareResult.getBlueprintComparisonErrors()).toContainEqual(languageTextfieldUnexpected);
+
+    const languageEmailUnexpected = new ComparisonError(
+      'oco01',
+      ComparisonErrorType.UNEXPECTED_KEY_IN_REAL_OBJECT,
+      new CedarJsonPath(
+        JsonSchema.properties,
+        '#002 Element with three fields',
+        JsonSchema.properties,
+        'Email',
+        JsonSchema.properties,
+        JsonSchema.atLanguage,
+      ),
+    );
+    expect(compareResult.getBlueprintComparisonErrors()).toContainEqual(languageEmailUnexpected);
+  });
+});

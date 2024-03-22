68942b1e100084e6b20ad435954cf164af6d8199
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-03-22 13:15:21 -0700

    New test files

---
+++ test/resources/templates/027/template-027.spec.ts
@@ -0,0 +1,38 @@
+import { CedarWriters, JSONTemplateReader, JSONTemplateWriter } from '../../../../src';
+import { ParsingResult } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ParsingResult';
+import { TestUtil } from '../../../TestUtil';
+import { ComparisonError } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ComparisonError';
+import { ComparisonErrorType } from '../../../../src/org/metadatacenter/model/cedar/util/compare/ComparisonErrorType';
+import { JsonPath } from '../../../../src/org/metadatacenter/model/cedar/util/path/JsonPath';
+import { CedarModel } from '../../../../src/org/metadatacenter/model/cedar/constants/CedarModel';
+
+xdescribe('JSONTemplateReader - template-027', () => {
+  test('reads template witch annotations', () => {
+    const templateSource = TestUtil.readTestResourceAsString('templates/027', 'template-027.json');
+    const reader: JSONTemplateReader = JSONTemplateReader.getStrict();
+    const jsonTemplateReaderResult = reader.readFromString(templateSource);
+    expect(jsonTemplateReaderResult).not.toBeNull();
+    const parsingResult = jsonTemplateReaderResult.parsingResult;
+    expect(parsingResult.wasSuccessful()).toBe(true);
+
+    // TestUtil.p(jsonTemplateReaderResult.template);
+
+    const writers: CedarWriters = CedarWriters.getStrict();
+    const writer: JSONTemplateWriter = writers.getJSONTemplateWriter();
+
+    const compareResult: ParsingResult = JSONTemplateReader.getRoundTripComparisonResult(jsonTemplateReaderResult, writer);
+
+    TestUtil.p(compareResult);
+    // TestUtil.p(writer.getAsJsonNode(jsonTemplateReaderResult.template));
+
+    expect(compareResult.wasSuccessful()).toBe(false);
+    expect(compareResult.getBlueprintComparisonErrorCount()).toBe(1);
+
+    const uiPagesMissing = new ComparisonError(
+      'oco02',
+      ComparisonErrorType.MISSING_KEY_IN_REAL_OBJECT,
+      new JsonPath(CedarModel.ui, CedarModel.pages),
+    );
+    expect(compareResult.getBlueprintComparisonErrors()).toContainEqual(uiPagesMissing);
+  });
+});

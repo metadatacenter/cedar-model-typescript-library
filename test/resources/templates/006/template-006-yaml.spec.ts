b7c7664d145e22d4a753816713afd26400ab6f60
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-03-27 16:54:25 -0700

    YAML files regenerated with the cedar-artifact-library

--- test/resources/templates/006/template-006-yaml.spec.ts
+++ test/resources/templates/006/template-006-yaml.spec.ts
@@ -1,9 +1,9 @@
 import { CedarWriters, JSONTemplateReader, YAMLTemplateWriter } from '../../../../src';
 import { TestUtil } from '../../../TestUtil';
 
-describe('YAMLTemplateWriter - template-005', () => {
+describe('YAMLTemplateWriter - template-006', () => {
   test('read a JSON template, and writes it as YAML', () => {
-    const artifactSource = TestUtil.readTestResourceAsString('templates/005', 'template-005.json');
+    const artifactSource = TestUtil.readTestResourceAsString('templates/006', 'template-006.json');
     const reader: JSONTemplateReader = JSONTemplateReader.getStrict();
     const jsonTemplateReaderResult = reader.readFromString(artifactSource);
     expect(jsonTemplateReaderResult).not.toBeNull();
@@ -17,29 +17,26 @@ describe('YAMLTemplateWriter - template-005', () => {
     // console.log(stringified);
     const expectedSerialization = `
 type: template
-id: https://repo.metadatacenter.org/templates/a4b6bc3a-dc47-4af1-9b7b-61fbdf1c4dce
-name: Template with multi field
+id: https://repo.metadatacenter.org/templates/c395555a-eb46-4e66-8bd0-5eba2e98371a
+name: Template with simple link
 status: bibo:draft
 version: 0.0.1
 modelVersion: 1.6.0
-createdOn: 2024-02-29T00:03:54-08:00
+createdOn: 2024-02-29T00:22:05-08:00
 createdBy: https://metadatacenter.org/users/c7dcc3ca-55fe-4ca8-b448-ab110bfe4c95
-lastUpdatedOn: 2024-02-29T00:03:54-08:00
+lastUpdatedOn: 2024-02-29T00:22:05-08:00
 modifiedBy: https://metadatacenter.org/users/c7dcc3ca-55fe-4ca8-b448-ab110bfe4c95
 children:
-  - multiple: true
-    minItems: 0
-    maxItems: 7
-    type: templateField
-    id: https://repo.metadatacenter.org/template-fields/16ae272a-364c-4770-a6d2-f9c2685de5c4
-    name: Textfield
+  - type: templateField
+    id: https://repo.metadatacenter.org/template-fields/c10d0f70-0e5d-405c-9f84-fafa7693cc63
+    name: Linkfield
     description: Help Text
     modelVersion: 1.6.0
-    inputType: textfield
+    inputType: link
     datatype: xsd:string
-    createdOn: 2024-02-29T00:03:54-08:00
+    createdOn: 2024-02-29T00:22:05-08:00
     createdBy: https://metadatacenter.org/users/c7dcc3ca-55fe-4ca8-b448-ab110bfe4c95
-    lastUpdatedOn: 2024-02-29T00:03:54-08:00
+    lastUpdatedOn: 2024-02-29T00:22:05-08:00
     modifiedBy: https://metadatacenter.org/users/c7dcc3ca-55fe-4ca8-b448-ab110bfe4c95
 `.trim();
     expect(stringified).toEqual(expectedSerialization);

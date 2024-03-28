dabd3443fff199678e2ae561789d290cd7887d5b
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-03-28 14:09:57 -0700

    Rename yaml files

--- regenerate-yaml-files.bash
+++ regenerate-yaml-files.bash
@@ -3,19 +3,19 @@ pushd ${CEDAR_HOME}/cedar-artifact-library
 
 for i in {1..4}; do
   num=$(printf "%03d" $i)
-  mvn exec:java@artifact2yaml -Dexec.args="-x -fsf ../cedar-model-typescript-library/test/resources/fields/${num}/field-${num}.json -y ../cedar-model-typescript-library/test/resources/fields/${num}/field-${num}.yaml"
+  mvn exec:java@artifact2yaml -Dexec.args="-x -fsf ../cedar-model-typescript-library/test/resources/fields/${num}/field-${num}.json -y ../cedar-model-typescript-library/test/resources/fields/${num}/field-${num}-java-lib.yaml"
 done
 
 for i in {1..4}; do
   num=$(printf "%03d" $i)
-  mvn exec:java@artifact2yaml -Dexec.args="-x -esf ../cedar-model-typescript-library/test/resources/elements/${num}/element-${num}.json -y ../cedar-model-typescript-library/test/resources/elements/${num}/element-${num}.yaml"
+  mvn exec:java@artifact2yaml -Dexec.args="-x -esf ../cedar-model-typescript-library/test/resources/elements/${num}/element-${num}.json -y ../cedar-model-typescript-library/test/resources/elements/${num}/element-${num}-java-lib.yaml"
 done
 
 for i in {1..30}; do
   num=$(printf "%03d" $i)
-  mvn exec:java@artifact2yaml -Dexec.args="-x -tsf ../cedar-model-typescript-library/test/resources/templates/${num}/template-${num}.json -y ../cedar-model-typescript-library/test/resources/templates/${num}/template-${num}.yaml"
+  mvn exec:java@artifact2yaml -Dexec.args="-x -tsf ../cedar-model-typescript-library/test/resources/templates/${num}/template-${num}.json -y ../cedar-model-typescript-library/test/resources/templates/${num}/template-${num}-java-lib.yaml"
 done
 
-mvn exec:java@artifact2yaml -Dexec.args="-x -tsf ../cedar-model-typescript-library/test/resources/templates/101/template-101.json -y ../cedar-model-typescript-library/test/resources/templates/101/template-101.yaml"
+mvn exec:java@artifact2yaml -Dexec.args="-x -tsf ../cedar-model-typescript-library/test/resources/templates/101/template-101.json -y ../cedar-model-typescript-library/test/resources/templates/101/template-101-java-lib.yaml"
 
 popd
